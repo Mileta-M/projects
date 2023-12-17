b4b99ef9d1eae87c2e7ebfe7edf4939678dddc29
Mileta-M <141850620+Mileta-M@users.noreply.github.com>
2023-12-17 18:42:33 +0100

    Add files via upload

---
+++ PracticeSoftwareTesting/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    watchForFileChanges: false
+    //testIsolation: false
+  },
+});
